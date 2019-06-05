c78aceb44aeb820a90ba696d48e01d8f62db443d
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-06-05 09:29:18 -0700

    Update StartScreenGUI.java

--- src/matchgame/StartScreenGUI.java
+++ src/matchgame/StartScreenGUI.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package matchgame;
 
-/**
- *
- * @author shann
- */
 public class StartScreenGUI extends javax.swing.JFrame {
 
     /**
